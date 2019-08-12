5c3a25404e38f89d3ea54c05f069ab4860395015
John Donaldson <jwrag@hotmail.com>
2019-08-12 14:25:30 +0100

    Added scss

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+});
+
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/master.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
